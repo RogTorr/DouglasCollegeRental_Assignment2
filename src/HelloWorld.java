ec0798d5fff2a484770c3fc9a443210ee90e06a1
Kiyohara <kiyohara@live.com>
2019-02-01 16:45:04 -0800

    Modify HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld extends JPanel{
 
 	public static void main(String[] args) {
 
-		JFrame frame = new JFrame("Hello World");
+		JFrame frame = new JFrame("Hello World!!!");
 		frame.setVisible(true);
 		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
